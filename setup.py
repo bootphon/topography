3687639679e670a1974ecac371d370a942a0ced1
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-05-09 15:53:12 +0200

    final fix ?

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="topography",
-    packages=["topography"],
+    packages=find_packages(),
     version="0.01",
     author="Maxime Poli",
     author_email="mpoli.pro@gmail.com",
