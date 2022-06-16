f69b9fb876f02b14b993f5ebd80403d7b80ffedf
Maxime Poli <44207491+mxmpl@users.noreply.github.com>
2022-06-16 17:31:15 +0200

    Revert "Add TopographicLoss and TopographicModel"

--- topography/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core members of this library: the TopographicLoss to be used
-in conjunction with the TopographicModel.
-"""
-from topography.core.loss import TopographicLoss, topographic_loss
-from topography.core.model import TopographicModel
-
-__all__ = ["TopographicLoss", "TopographicModel", "topographic_loss"]
