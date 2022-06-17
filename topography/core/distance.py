95ecb1e51c03acc75b5d3afd00dc1a461b1f95d8
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-06-17 12:46:35 +0200

    resolve conversations

---
+++ topography/core/distance.py
@@ -0,0 +1,110 @@
+"""Assign positions in 2D surfaces or 3D volumes, and compute
+the inverse distance between those positions.
+"""
+import math
+from typing import Callable, Dict
+
+import torch
+
+
+def hypercube(
+    num_points: int,
+    dimension: int,
+    lower_bound: float = 0,
+    upper_bound: float = 1,
+) -> torch.Tensor:
+    """Creates `num` positions in a cube of `dimension` dimensions.
+    If there exists an integer `num_axis` such that
+    `num_axis**dimension == num_points`, the positions will be distributed
+    uniformly in the cube.
+    Else, the closes `num_axis` will be taken, `num_axis**dimension`
+    positions are computed and only the first `num` positions are
+    returned.
+
+    Parameters
+    ----------
+    num_points : int
+        Number of positions to create.
+    dimension : int
+        Dimension of the positions. 1D for a line, 2D for a square grid,
+        3D for a grid in a cube, etc.
+    low : float, optional
+        Lower bound of each coordinate in the cube, by default 0.
+    high : float, optional
+        Higher bound of each coordinate in the cube, by default 1.
+
+    Returns
+    -------
+    torch.Tensor
+        Tensor of positions, of shape (`num_points`, `dimension`).
+    """
+    num_axis = int(math.ceil(num_points ** (1 / dimension)))
+    coords = [
+        torch.linspace(lower_bound, upper_bound, num_axis)
+        for _ in range(dimension)
+    ]
+    return (
+        torch.cat(torch.meshgrid(*coords, indexing="xy"))
+        .reshape(dimension, -1)
+        .T[:num_points]
+    )
+
+
+_DISTANCES: Dict[str, Callable] = {
+    "euclidean": lambda coords: torch.cdist(coords, coords, p=2)
+}
+_POSITIONS: Dict[str, Callable] = {"hypercube": hypercube}
+
+
+def inverse_distance(
+    out_channels: Dict[str, int],
+    dimension: int = 2,
+    norm: str = "euclidean",
+    position_scheme: str = "hypercube",
+) -> Dict[str, torch.Tensor]:
+    """Compute the inverse distance matrices to be used in the topographic
+    loss. Function called when creating the TopographicModel.
+    Each matrix is upper triangular in order to remove
+    redundant distances.
+
+    Parameters
+    ----------
+    out_channels : Dict[str, int]
+        Dictionnary containing the number of output channels
+        of each Conv2d layer.
+    dimension : int, optional
+        Dimension of the position assigned to each channel
+        of each Conv2d layer, by default 2.
+    norm : str, optional
+        Which norm between positions to use. Must be "euclidean",
+        by default "euclidean".
+    position_scheme : str, optional
+        How to assign positions. Must be "hypercube", by default "hypercube".
+
+    Returns
+    -------
+    Dict[str, torch.Tensor]
+        Dictionnary of inverse distance matrices for each Conv2d layer.
+    """
+    inv_dist = {}
+    assign_position = _POSITIONS[position_scheme]
+    distance = _DISTANCES[norm]
+    for layer, num_channels in out_channels.items():
+        coords = assign_position(num_channels, dimension)
+        inv_dist[layer] = torch.triu(1 / (distance(coords) + 1), diagonal=1)
+    return inv_dist
+
+
+if __name__ == "__main__":  # pragma: no cover
+    # Simple check in order to visualize how the positions are distributed.
+    import matplotlib.pyplot as plt
+
+    square_x, square_y = hypercube(64, 2).T
+    plt.scatter(square_x, square_y)
+    plt.show()
+
+    cube_x, cube_y, cube_z = hypercube(256, 3).T
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter(cube_x, cube_y, cube_z, marker="o")
+    plt.show()
---
+++ topography/core/model.py
@@ -0,0 +1,112 @@
+"""Topographic model. Add positions to all Conv2d layers in a base model
+and try to enforce topographic organization.
+The inverse pairwise distances between channels are stored under
+the `inverse_distance` buffer.
+"""
+import copy
+from collections import OrderedDict
+from operator import attrgetter
+from typing import Callable
+
+import torch
+from torch import nn
+
+from topography.core.distance import inverse_distance
+
+
+class TopographicModel(nn.Module):
+    """Class to introduce topographic organization in PyTorch models
+    with Conv2d layers."""
+
+    def __init__(
+        self,
+        model: nn.Module,
+        dimension: int = 2,
+        norm: str = "euclidean",
+        position_scheme: str = "hypercube",
+    ) -> None:
+        """Creates the model. It is the same as the original model,
+        but adds an `activations` field that records outputs
+        of each Conv2d layer. Also adds an `inverse_distance`
+        buffer to them.
+
+        Parameters
+        ----------
+        model : nn.Module
+            Original model. Must have Conv2d layers.
+        dimension : int, optional
+            Dimension of the position assigned to each channel
+            of each Conv2d layer, by default 2.
+        norm : str, optional
+            Which norm between positions to use. Must be "euclidean",
+            by default "euclidean".
+        position_scheme : str, optional
+            How to assign positions. Must be "hypercube", by default
+            "hypercube".
+        """
+        super().__init__()
+        self.model = copy.deepcopy(model)
+
+        names, conv_layers = zip(
+            *list(
+                filter(
+                    lambda named_module: isinstance(named_module[1], nn.Conv2d),
+                    self.model.named_modules(),
+                )
+            )
+        )
+        out_channels = OrderedDict(
+            zip(names, [layer.out_channels for layer in conv_layers])
+        )
+
+        self.activations = OrderedDict()
+        inv_dist = inverse_distance(
+            out_channels, dimension, norm, position_scheme
+        )
+
+        def get_activation(name: str) -> Callable[..., None]:
+            def hook(_, __, out: torch.Tensor) -> None:
+                self.activations[name] = out
+
+            return hook
+
+        self._conv_layer_names = names
+        for name, layer in zip(names, conv_layers):
+            layer.register_buffer("inverse_distance", inv_dist[name])
+            layer.register_forward_hook(get_activation(name))
+
+    @property
+    def inverse_distance(self) -> OrderedDict:
+        """Get the inverse distances between the channels positions
+        of each Conv2d layer.
+        For each layer, a buffer "inverse_distance" is registered, and we
+        retrieve them in this method.
+        It is implemented that way in order for them to be inside
+        the `state_dict` and to move to the same device as the model when
+        needed.
+
+        Returns
+        -------
+        OrderedDict
+            The dictionnary of inverse distances.
+        """
+        inv_dist = OrderedDict()
+        for name in self._conv_layer_names:
+            inv_dist[name] = attrgetter(name)(self.model).inverse_distance
+        return inv_dist
+
+    def forward(self, inp: torch.Tensor) -> torch.Tensor:
+        """Defines the computation performed by the TopographicModel:
+        it is the one done by the base model.
+
+        Parameters
+        ----------
+        inp : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output computed by the base model.
+        """
+        return self.model(inp)
