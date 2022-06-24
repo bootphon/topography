e6e70cc25eb3cc1baca51950cd9f5344325d5222
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-06-24 09:51:40 +0200

    Update launch scripts

--- scripts/launch_parallel.sh
+++ scripts/launch_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jobs_in_parallel=64
+jobs_in_parallel=128
 
 if [ ! -f "$1" ]
 then
@@ -14,4 +14,8 @@ n_lines=$(grep -c '^' "$1")
 # Use file name for job name
 job_name=$(basename "$1" .txt)
 
-sbatch --array=1-${n_lines}%${jobs_in_parallel} --job-name ${job_name} $(dirname "$0")/launch_job.sh "$1"
+sbatch --array=1-${n_lines}%${jobs_in_parallel} \
+--job-name ${job_name} $(dirname "$0")/launch_job.sh \
+--output=$WORK/logs/%x_%j.out \
+--error=$WORK/logs/%x_%j.err \
+"$1"
