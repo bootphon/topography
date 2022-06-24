52aa33fbf9637860abc58856f05a496ae4db6982
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-06-24 14:07:59 +0200

    better output and err names

--- scripts/launch_parallel.sh
+++ scripts/launch_parallel.sh
@@ -15,7 +15,7 @@ n_lines=$(grep -c '^' "$1")
 job_name=$(basename "$1" .txt)
 
 sbatch --array=1-${n_lines}%${jobs_in_parallel} \
---output=$WORK/logs/%x_%j.out \
---error=$WORK/logs/%x_%j.err  \
+--output=$WORK/logs/%x_%A_%a.out \
+--error=$WORK/logs/%x_%A_%a.err  \
 --job-name ${job_name} \
 $(dirname "$0")/launch_job.sh "$1"
