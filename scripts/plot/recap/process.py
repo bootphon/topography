7b3d7540cc77d71a39ad268ce5ac4df40cc6c2a8
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-07-26 13:43:27 +0200

    recap plots and metrics

---
+++ scripts/plot/recap/process.py
@@ -0,0 +1,138 @@
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from topography.utils import tensorboard_to_dataframe
+
+
+def plot_processed_recap(
+    dataframe: pd.DataFrame, path: Path, overwrite: bool
+) -> None:
+    """Process the recap dataframe. Plot the test accuracy as a function
+    of lambda, for the different models and dimension of the positions.
+
+    Parameters
+    ----------
+    dataframe : pd.DataFrame
+        Dataframe containing the recap of the experiments.
+    path : Path
+        Path to save the recap plots.
+    overwrite : bool
+        Whether to overwrite existing files or not.
+    """
+    if path.exists() and not overwrite:
+        return
+    df = dataframe.dropna(axis=0)
+    df = df[df.model != "alexnet"]
+    prod = [
+        (model, num_classes)
+        for model in df.model.unique()
+        for num_classes in df[df.model == model].num_classes.unique()
+    ]
+    nrows = int(np.sqrt(len(prod)))
+    ncols = len(prod) // nrows
+    ncols += 1 if ncols * nrows < len(prod) else 0
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(15, 15))
+    for k, (model, num_classes) in enumerate(prod):
+        i, j = k % nrows, k // nrows
+
+        df_model = df[(df.model == model) & (df.num_classes == num_classes)]
+        reference = dataframe[
+            (dataframe.model == model)
+            & (dataframe.num_classes == num_classes)
+            & (dataframe.topographic == False)
+        ].test_acc.mean()
+        if not np.isnan(reference):
+            ax[i, j].hlines(
+                reference,
+                df_model.lambd.min(),
+                df_model.lambd.max(),
+                label="ref",
+                colors="k",
+                zorder=1,
+            )
+
+        for dim in sorted(df.dimension.unique()):
+            subdf = df_model[df_model.dimension == dim]
+            ax[i, j].scatter(
+                subdf.lambd,
+                subdf.test_acc,
+                label=f"dim={int(dim)}",
+                alpha=0.3,
+                edgecolors="none",
+                linewidths=2,
+                s=50,
+                zorder=2,
+            )
+        ax[i, j].legend(loc="lower left")
+        ax[i, j].set_title(f"{model}, {num_classes}")
+        ax[i, j].set_xscale("log")
+        ax[i, j].set_xlabel("lambda")
+        ax[i, j].set_ylabel("Test acc")
+    fig.savefig(path)
+    plt.close()
+
+
+def add_columns_to_recap(
+    dataframe: pd.DataFrame, path: Path, overwrite: bool
+) -> None:
+    if path.exists() and not overwrite:
+        return
+    scale = []
+    for _, row in dataframe.iterrows():
+        tb_dir = Path(row.log).joinpath("tensorboard")
+        if not tb_dir.exists():
+            raise ValueError(
+                f"Current row has a non valid 'log' entry: {row.log}"
+            )
+        if len(list(tb_dir.glob("*"))) != 1:
+            raise ValueError(
+                "Tensorboard directory does not have only one "
+                + f"element, but {len(list(tb_dir.glob('*')))}: {tb_dir}."
+            )
+        tb_df = tensorboard_to_dataframe(str(next(tb_dir.glob("*"))))
+
+        # Add column with lambda*topo_loss / cross_entropy
+        if row.topographic:
+            subdf = tb_df[tb_df.step == 1.0]
+            cross_entropy_loss = subdf[
+                subdf.metric == "train/extras/loss-cross-entropy"
+            ].value.to_numpy()[0]
+            topo_loss = subdf[
+                subdf.metric == "train/extras/loss-topographic"
+            ].value.to_numpy()[0]
+            scale.append(f"{row.lambd * topo_loss / cross_entropy_loss:0.3e}")
+        else:
+            scale.append(np.nan)
+    dataframe["scale"] = scale
+    dataframe.to_csv(path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output directory containing the recaps.",
+    )
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        help="Whether to overwrite existing files or not.",
+    )
+    args = parser.parse_args()
+    output, overwrite = Path(args.output).resolve(), args.overwrite
+
+    if not output.exists():
+        raise ValueError("Given argument 'output' is not a valid directory.")
+    dataframe = pd.read_csv(output.joinpath("full_recap.csv"))
+    plot_processed_recap(
+        dataframe, output.joinpath("processed_recap.pdf"), overwrite
+    )
+    add_columns_to_recap(
+        dataframe, output.joinpath("enhanced_recap.csv"), overwrite
+    )
---
+++ scripts/plot/recap/run.py
@@ -0,0 +1,125 @@
+"""Script to recap the metrics and plots of the different experiments."""
+import argparse
+import dataclasses
+import itertools
+import json
+from pathlib import Path
+
+import pandas as pd
+from PIL import Image
+
+
+@dataclasses.dataclass(frozen=True)
+class RecapConfig:
+    experiments: Path  # Path to the folder containing all the experiments.
+    output: Path  # Path to the output folder, where the recaps will be.
+    # Start of the experiments path, used for parsing the file names.
+    start_path: str = "experiments"
+    overwrite: bool = False  # Whether to overwrite existing files or not.
+
+
+def main(config: RecapConfig) -> None:
+    """Build the recaps.
+
+    Parameters
+    ----------
+    config : RecapConfig
+        Specified configuration.
+    """
+    recaps = []
+    config.output.mkdir(exist_ok=True)
+    # Key function for itertools.groupby
+    # Used to group plots together: plots that are to be grouped
+    # have their category specified at the beginning of the file name
+    # until a "_" is reached.
+    keyfunc = lambda path: path.name.split("_")[0]
+    for path in config.experiments.rglob("tensorboard"):
+        parent = path.parent
+        out = config.output.joinpath(  # Directory for the current experiment
+            str(parent).split(config.start_path)[1].replace("/", "_").strip("_")
+        )
+        out.mkdir(exist_ok=True)
+        print(str(out))
+
+        # Make a recap of the plots
+        if parent.joinpath("plot").exists():
+            img_root = sorted(parent.joinpath("plot").glob("*.png"))
+            for key, group in itertools.groupby(img_root, keyfunc):
+                if not out.joinpath(f"{key}.pdf").exists() or config.overwrite:
+                    images = [Image.open(img).convert("RGB") for img in group]
+                    images[0].save(
+                        out.joinpath(f"{key}.pdf"),
+                        save_all=True,
+                        append_images=images[1:],
+                    )
+        try:
+            # Configuration file
+            with open(
+                parent.joinpath("environment/config.json"),
+                "r",
+                encoding="utf-8",
+            ) as file:
+                recap = json.load(file)
+            with open(
+                out.joinpath("config.json"), "w", encoding="utf-8"
+            ) as file:
+                json.dump(recap, file)
+            # Recover the test accuracy
+            test_acc = None
+            with open(
+                parent.joinpath("summary.log"), "r", encoding="utf-8"
+            ) as file:
+                lines = file.readlines()
+                for line in lines:
+                    if "test" in line and "acc" in line:
+                        for part in line.strip().split(", "):
+                            if part.startswith("acc"):
+                                test_acc = float(part.removeprefix("acc"))
+                        break
+            with open(
+                out.joinpath("summary.log"), "w", encoding="utf-8"
+            ) as file:
+                file.write("".join(lines))
+            recap["test_acc"] = test_acc
+            recaps.append(recap)
+        except FileNotFoundError as error:
+            print(str(error))
+    # CSV containing the recap of the results
+    dataframe = pd.DataFrame(recaps)
+    csv_path = config.output.joinpath("full_recap.csv")
+    if not csv_path.exists() or config.overwrite:
+        dataframe.to_csv(csv_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-x",
+        "--experiments",
+        type=str,
+        required=True,
+        help="Experiments directory",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Output directory containing the recaps.",
+    )
+
+    parser.add_argument(
+        "--overwrite",
+        action="store_true",
+        help="Whether to overwrite existing files or not.",
+    )
+
+    parser.add_argument("--start_path", type=str, default="experiments")
+
+    args = parser.parse_args()
+    experiments = Path(args.experiments).resolve()
+    output = Path(args.output).resolve()
+    config = RecapConfig(
+        experiments=experiments, output=output, overwrite=args.overwrite
+    )
+    main(config)
