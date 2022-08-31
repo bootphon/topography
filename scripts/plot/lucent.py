475cde25f56248c9b996621daa63fbbae0a0a2b4
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-08-31 13:58:44 +0200

    Lucent script

---
+++ scripts/plot/lucent.py
@@ -0,0 +1,179 @@
+"""Script to visualize channels by finding the highest activating images,
+using the Lucent library.
+"""
+import argparse
+import json
+import random
+from collections import defaultdict
+from copy import deepcopy
+from pathlib import Path
+from typing import Callable, Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from lucent.optvis import param, render
+from PIL import Image
+from torchvision import datasets
+from tqdm.auto import tqdm
+
+from topography import TopographicModel, models
+from topography.core.distance import hypercube
+from topography.utils.data import BirdDCASE, SpeechCommands
+
+# Cf https://distill.pub/2017/feature-visualization/#preconditioning
+# in order to understand these parameters and how alternative
+# parametrizations can produce better results.
+FFT: bool = True
+DECORRELATE: bool = True
+
+
+def run(
+    model: TopographicModel, plotdir: Path, param_f: Optional[Callable] = None
+) -> None:
+    """Find the highest acitvating images for every channel of every
+    Conv2d layer with topography.
+
+    Parameters
+    ----------
+    model : TopographicModel
+        The topographic model.
+    plotdir : Path
+        The directory where to save the resulting images.
+    param_f : Optional[Callable]
+        Function used in `lucent.optvis.render.render_vis`.
+        Quote from `render_vis`:
+        ```
+            # param_f is a function that should return two things
+            # params - parameters to update, which we pass to the optimizer
+            # image_f - a function that returns an image as a tensor
+        ```
+    """
+
+    plotdir.mkdir(exist_ok=True, parents=True)
+    for layer_name, inv_dist in model.inverse_distance.items():
+        out_channels = inv_dist.shape[0]
+        layer = layer_name.replace(".", "_")
+        for channel in tqdm(range(out_channels), desc="Render vis"):
+            render.render_vis(
+                model,
+                f"model_{layer}:{channel}",
+                param_f,
+                show_image=False,
+                save_image=True,
+                image_name=plotdir / f"{layer}-{channel}.png",
+                progress=False,
+            )
+
+
+def process(plotdir: Path) -> None:
+    """Arange the resulting images according to the 2D grid topography,
+    for every layer.
+
+    Parameters
+    ----------
+    plotdir : Path
+        Directory where the resulting images were saved.
+        The final visualization will be saved in `plotdir.parent`.
+    """
+    imgs = defaultdict(dict)
+
+    for path in plotdir.glob("*.png"):
+        layer_name, channel = path.stem.split("-")
+        imgs[layer_name][int(channel)] = deepcopy(Image.open(path))
+
+    for current_layer in tqdm(imgs.keys(), desc="Plot grid"):
+        num_channels = len(imgs[current_layer])
+
+        positions = hypercube(
+            num_points=num_channels,
+            dimension=2,
+            integer_positions=True,
+        )
+        num_axis = positions.max().item() + 1
+
+        fig, ax = plt.subplots(
+            nrows=num_axis,
+            ncols=num_axis,
+            figsize=(2 * num_axis, 2 * num_axis),
+        )
+
+        for channel, (i, j) in enumerate(positions):
+            ax[num_axis - 1 - j, i].imshow(imgs[current_layer][channel])
+        for i in range(ax.shape[0]):
+            for j in range(ax.shape[1]):
+                ax[i, j].axis("off")
+
+        fig.suptitle(current_layer)
+        fig.savefig(plotdir.parent / f"{current_layer}.pdf")
+        fig.savefig(plotdir.parent / f"{current_layer}.png")
+        plt.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--log", type=str, help="Logging directory")
+    parser.add_argument("--seed", type=int, default=0, help="Random seed")
+    args = parser.parse_args()
+    logdir = Path(args.log).resolve()
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    with open(logdir / "environment/config.json", "r") as f:
+        config = json.load(f)
+
+    dataset = config["dataset"]
+    if dataset.startswith("cifar"):
+        num_classes = int(dataset.removeprefix("cifar"))
+        in_channels = 3
+        dataset = datasets.CIFAR10 if num_classes == 10 else datasets.CIFAR100
+        inp = dataset(root=config["data"], train=True, download=False)[0][0]
+    elif dataset == "speechcommands":
+        num_classes, in_channels = 35, 1
+        inp = SpeechCommands(config["data"], subset="training")[0][0]
+    elif dataset == "birddcase":
+        num_classes, in_channels = 2, 1
+        inp = BirdDCASE(config["data"], subset="training")[0][0]
+    else:
+        raise ValueError(f"Wrong dataset {dataset}")
+    param_f = lambda: param.image(
+        w=inp.shape[0],
+        h=inp.shape[1],
+        channels=in_channels,
+        fft=FFT,
+        decorrelate=DECORRELATE,
+    )
+
+    model_name = config["model"]
+    base_model = getattr(models, model_name)(
+        num_classes=num_classes, in_channels=in_channels
+    )
+    state_dict = torch.load(
+        sorted((logdir / "checkpoints").glob("*.model"))[-1],
+        map_location=device,
+    )
+    topo_names = models.topographic_layer_names(model_name)
+    if config["topographic"]:
+        if config["dimension"] != 2 or config["position_scheme"] != "hypercube":
+            raise ValueError(
+                "If the model is topographic, it must be using"
+                + " a 2D regular grid topography."
+            )
+        model = (
+            TopographicModel(base_model, topographic_layer_names=topo_names)
+            .eval()
+            .to(device)
+        )
+        model.load_state_dict(state_dict)
+    else:
+        base_model.load_state_dict(state_dict)
+        model = TopographicModel(base_model, topo_names).eval().to(device)
+
+    run(model, logdir / "plot/lucent", param_f)
+    process(logdir / "plot/lucent")
