bc0956d68f59da71954538e9c31ad526be92ff18
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-09-05 10:09:30 +0200

    Concatenante jobs script

--- scripts/create_jobs.sh
+++ scripts/create_jobs.sh
@@ -1,7 +1,10 @@
 for dataset in birddcase cifar speechcommands
 do
-    for seed in 1 2 3 4
+    for seed in 0 1 2 3 4
     do
         python $dataset/create_jobs.py -w $WORK --seed $seed
     done
 done
+
+cat birddcase*.txt cifar*.txt speechcommands*.txt > all.txt
+rm birddcase*.txt cifar*.txt speechcommands*.txt
