67ac9ebafc138dba7deb6787a3764e6ef141313c
mxmpl <44207491+mxmpl@users.noreply.github.com>
2022-10-27 10:54:03 +0200

    move create_jobs

--- scripts/training/create_jobs.py
+++ scripts/training/create_jobs.py
@@ -1,56 +1,59 @@
-"""Simple script to create jobs to run on SpeechCommands."""
+"""Simple script to create all jobs to run."""
 import argparse
 from itertools import product
 from pathlib import Path
 
-OUTPUT = "speechcommands"
+DATASETS = ["speechcommands", "birddcase", "cifar10", "cifar100"]
+SEEDS = [0, 1, 2, 3, 4]
+
+MODELS = ["resnet18", "vgg16_bn", "densenet121"]
+DIMENSIONS = [1, 2, 3]
+NORMS = ["euclidean", "l1"]
+POSITION_SCHEMES = ["hypercube", "nested", "hypersphere"]
+LAMBDAS = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 0.1, 0.5, 1, 5]
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-w", "--workdir", type=str, help="Work directory", required=True
-    )
-    parser.add_argument(
-        "-d",
-        "--dataset",
-        type=str,
-        choices=["cifar", "birddcase", "speechcommands"],
-    )
-    parser.add_argument("-s", "--seed", type=int, help="Random seed", default=0)
+    parser.add_argument("experiments", type=str, help="Experiments directory")
+    parser.add_argument("data", type=str, help="Data directory")
     args = parser.parse_args()
 
-    workdir = Path(args.workdir).resolve()
-    jobs = Path(f"./{OUTPUT}_{args.seed}.txt").resolve()
-    script = Path(__file__).resolve().parent / "run.py"
-    logdir = workdir / "experiments"
-    datadir = workdir / "data"
+    expdir = Path(args.experiments).resolve()
+    datadir = Path(args.data).resolve()
+    scriptdir = Path(__file__).resolve().parent
 
-    models = ["resnet18", "vgg16_bn", "densenet121"]
-    dimensions = [1, 2, 3]
-    norms = ["euclidean", "l1"]
-    position_schemes = ["hypercube", "nested", "hypersphere"]
-    lambdas = [1e-5, 5e-5, 1e-4, 5e-4, 1e-3, 5e-3, 1e-2, 5e-2, 0.1, 0.5, 1, 5]
-
-    with open(jobs, "w", encoding="utf-8") as file:
-        for model in models:
-            path = f"speechcommands/{model}/base"
-            file.write(
-                f"python {script} --log {logdir / path}"
-                f" --data {datadir} --seed {args.seed} --model {model}\n"
+    jobs = []
+    for dataset, seed in product(DATASETS, SEEDS):
+        if dataset.startswith("cifar"):
+            dataset, num_classes = "cifar", int(dataset.removeprefix("cifar"))
+        script = scriptdir / f"{dataset}.py"
+        for model in MODELS:
+            path = f"{dataset}/{model}/base"
+            job = (
+                f"python {script} --log {expdir / path}"
+                f" --data {datadir} --seed {seed} --model {model}"
             )
+            if dataset == "cifar":
+                job += f" --num_classes {num_classes}"
+            jobs.append(job)
         for model, dim, norm, lambd, position_scheme in product(
-            models, dimensions, norms, lambdas, position_schemes
+            MODELS, DIMENSIONS, NORMS, LAMBDAS, POSITION_SCHEMES
         ):
             if position_scheme == "hypersphere" and dim == 1:
                 continue  # No sphere in dimension 1
             path = (
-                f"speechcommands/{model}/{position_scheme}/"
+                f"{dataset}/{model}/{position_scheme}/"
                 + f"dimension_{dim}/lambda_{lambd}/norm_{norm}"
             )
-            file.write(
-                f"python {script} --log {logdir / path}"
-                f" --data {datadir} --seed {args.seed} --model {model}"
+            job = (
+                f"python {script} --log {expdir / path}"
+                f" --data {datadir} --seed {seed} --model {model}"
                 f" --topographic --norm {norm}"
                 f" --position_scheme {position_scheme}"
-                f" --lambd {lambd} --dimension {dim}\n"
+                f" --lambd {lambd} --dimension {dim}"
             )
+            if dataset == "cifar":
+                job += f" --num_classes {num_classes}"
+            jobs.append(job)
+    Path("./jobs.txt").write_text("\n".join(jobs))
